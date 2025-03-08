03b588842d526bb6da3cd2b6ebd9913eedcb68da
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-03-08 10:48:48 +0100

    prima versione

---
+++ cicloEnumerativo.ml
@@ -0,0 +1,5 @@
+Printf.printf "Programma che implementa un countdown.\n\n"
+let rec loop i =
+  if i<0 then Printf.printf "%d" i
+  else begin (Printf.printf "%d\n" i); loop (i-1) end
+  in loop 10;;
