f5c36e2a37c862b62ddb4daec7db87d374e329f0
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-03-08 11:04:35 +0100

    correzioni varie

--- cicloEnumerativo.ml
+++ cicloEnumerativo.ml
@@ -1,5 +1,18 @@
-Printf.printf "Programma che implementa un countdown.\n\n";;
+(* Stampa un messaggio che descrive il programma *)
+Printf.printf "Programma che implementa un countdown mediante ciclo enumerativo.\n\n";;
+
+(* Definisce una funzione ricorsiva chiamata loop che accetta un argomento i *)
 let rec loop i =
-  if i<0 then Printf.printf "%d" i
-  else begin (Printf.printf "%d\n" i); loop (i-1) end
-  in loop 10;;
+  (* Controlla se i è minore di 0 *)
+  if i < 0 then 
+    (* Se i è minore di 0, stampa il valore di i *)
+    Printf.printf "%d" i
+  else begin 
+    (* Se i non è minore di 0, stampa il valore di i seguito da una nuova riga *)
+    Printf.printf "%d\n" i; 
+    (* Chiama ricorsivamente la funzione loop decrementando i di 1 *)
+    loop (i - 1) 
+  end
+
+(* Avvia il countdown chiamando la funzione loop con il valore iniziale 10 *)
+in loop 10;;
