65fa07571b0961f68f765793b5261c484b3dba64
Lorenzo Ricciardi <56824204+P1c1s@users.noreply.github.com>
2025-03-09 21:37:50 +0100

    Delete record.ml

--- record.ml
+++
@@ -1,5 +0,0 @@
-type persona = {nome : string; cognome : string; eta: int};;
-
-let mario = {nome = "Mario"; cognome = "Filini"; eta = 25};;
-
-Printf.printf "nome: %s\ncognome: %s\netà: %d\n" mario.nome mario.cognome mario.eta;;
