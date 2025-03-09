14d8da8f1c12f788468eacc0c179f7f4ea212774
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-03-09 15:05:48 +0100

    miglioramenti vari

--- calcolatriceInterattiva.ml
+++ calcolatriceInterattiva.ml
@@ -1,4 +1,4 @@
-Printf.printf "Programma che implementa un countdown mediante ciclo enumerativo.\n\n";;
+Printf.printf "Programma che implementa un countdown mediante ciclo enumerativo.\n";;
 
 let letturaComando() = 
     let x = Printf.printf "Comando: "; read_line()
@@ -10,10 +10,14 @@ let letturaNumeri() =
     (int_of_string x, int_of_string y);;
 
 let stampa() =
-    Printf.printf "1) Addizione\n";
-    Printf.printf "2) Sottrazione\n";
-    Printf.printf "3) Moltiplicazione\n";
-    Printf.printf "4) Divisione\n\n";;
+    Printf.printf "\n+---------Menu-------+\n";
+    Printf.printf "| 1) Addizione       |\n";
+    Printf.printf "| 2) Sottrazione     |\n";
+    Printf.printf "| 3) Moltiplicazione |\n";
+    Printf.printf "| 4) Divisione       |\n";
+    Printf.printf "| 5) Potenza         |\n";
+    Printf.printf "| 0) Esci            |\n";
+    Printf.printf "+--------------------+\n\n";;
 
 let addizione() = 
     let (x,y) = letturaNumeri() in
@@ -31,18 +35,23 @@ let divisione() =
     let (x,y) = letturaNumeri() in
     Printf.printf "%d/%d=%d\n" x y (x/y);;
 
+let potenza() = 
+    let (x,y) = letturaNumeri() in
+    Printf.printf "%d^%d=%f\n" x y (float_of_int x ** float_of_int y);;
+
 let errore() = Printf.printf "Comando non trovato\n";;
     
 let funzione x = match x with
-    | 1 -> addizione ()
-    | 2 -> sottrazione ()
-    | 3 -> moltiplicazione ()
-    | 4 -> divisione ()
+    | 1 -> addizione()
+    | 2 -> sottrazione()
+    | 3 -> moltiplicazione()
+    | 4 -> divisione()
+    | 5 -> potenza()
     | 0 -> Printf.printf "Uscita dal programma.\n"
-    | _ -> errore ();;
+    | _ -> errore();;
 
-let rec loop () =
-    stampa ();
+let rec loop() =
+    stampa();
     let comando = letturaComando () in 
     if comando <> 0 then begin funzione comando; loop () end else Printf.printf "Uscita dal programma\n\nCiao ciao!\n";;
 
