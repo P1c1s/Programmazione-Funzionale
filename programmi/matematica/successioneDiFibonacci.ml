370d8c28f7df4a095c95ebe60e0150e3f128a1c1
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-03-11 13:13:39 +0100

    ora la funizione fibonacci è parametrica

--- programmi/matematica/successioneDiFibonacci.ml
+++ programmi/matematica/successioneDiFibonacci.ml
@@ -1,12 +1,12 @@
 Printf.printf "Programma che stampa la sequenza di Fibonacci\n"
 
-let rec fibonacci (x,y,i) =
-  if i < 5  then 
+let rec fibonacci (x, y, contatore, max) =
+  if contatore < max  then 
     begin
       Printf.printf "%d - " (x+y);
-      fibonacci(y, x+y, i+1)
+      fibonacci(y, x+y, contatore+1, max)
     end
   else
     Printf.printf "%d \n" (x+y);;
 
-let () = fibonacci(0, 1, 1);;
+let () = fibonacci(0, 1, 1, 10);;
