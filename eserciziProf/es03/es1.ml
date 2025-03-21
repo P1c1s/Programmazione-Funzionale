5aa09d66cca07dd7084071f496e9fc5950b00b53
Michela Sicuranza <michela.sicuranza@gmail.com>
2025-03-21 11:13:30 +0100

    modifica es1

--- eserciziProf/es03/es1.ml
+++ eserciziProf/es03/es1.ml
@@ -2,4 +2,4 @@ exception BadDate
 
 let somma_ore (h1,m1) (h2, m2)= 
   if h1<0 || h2<0 || h1>23 || h2>23 || m1<0 || m2<0 || m1>59 || m2>59 then raise BadDate
-  else ((h1+h2)+((m1+m2)/60), (m1+m2) mod 60);;
+  else (((h1+h2)+((m1+m2)/60) mod 24), (m1+m2) mod 60);;
