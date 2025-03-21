65c38cfef14c925e4f51997fdca836e0a00564f5
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-03-21 11:44:31 +0100

    esercizio2 lorenzo

---
+++ eserciziProf/es03/ese2-lorenzo.ml
@@ -0,0 +1,14 @@
+exception StringaVuota
+
+let rec read_max max =
+  let input = read_line() in
+  if input = "" then
+    (Printf.printf "Massimo: %d\n" max; raise StringaVuota) 
+  else
+    if (int_of_string input) > max then
+      read_max (int_of_string input)
+    else
+      read_max max;;
+    
+
+read_max (int_of_string (read_line()));;
