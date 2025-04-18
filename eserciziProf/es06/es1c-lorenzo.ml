9bf8e4dda288839ecebde4324be649e6ed64429a
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-04-18 11:50:24 +0200

    forced

--- eserciziProf/es06/es1c-lorenzo.ml
+++ eserciziProf/es06/es1c-lorenzo.ml
@@ -1,7 +1,5 @@
 let pari n = (n mod 2) = 0;;
 
-let dropwhile predicato lista = 
-  let rec aux l = function
-    | [] -> l
-    | x::xs -> if predicato x = true then aux l xs else (x::xs)
-  in aux [] lista;; 
+let rec dropwhile predicato = function
+    | [] -> []
+    | x::xs -> if predicato x = true then dropwhile predicato xs else (x::xs);;
